82edf8825ef26abac4e04867a4d50b5756f11478
deblur99 <deblur99@kakao.com>
2023-08-08 16:18:21 +0900

    Initial Commit

---
+++ busalram_ios_native/busalram_ios_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  busalram_ios_nativeApp.swift
+//  busalram_ios_native
+//
+//  Created by 한현민 on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct busalram_ios_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
